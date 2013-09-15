ab33347b1fb1da36b8c8feb6e023c80514ab2cf1
lavi2088 <lavigupta@Lavis-Mac-mini.local>
2013-09-14 22:15:09 -0700

    Initial Commit

---
+++ OTCShort/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OTCShort
+//
+//  Created by Lavi Gupta on 14/09/13.
+//  Copyright (c) 2013 myriad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OTCShort/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OTCShort
+//
+//  Created by Lavi Gupta on 14/09/13.
+//  Copyright (c) 2013 myriad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
